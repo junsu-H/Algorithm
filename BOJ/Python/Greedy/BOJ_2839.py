7276b6ebdaa8aa19dc136b672c0490b1dd50da88
junsu hwang <junsu0325@naver.com>
2021-02-02 17:32:09 +0900

    Greedy

---
+++ BOJ/Python/Greedy/BOJ_2839.py
@@ -0,0 +1,20 @@
+import sys
+
+def solution():
+    n = int(sys.stdin.readline().rstrip())
+    
+    answer = 0
+    while True:
+        if (n % 5 == 0):
+            answer += (n//5)
+            print(answer)
+            break
+
+        n -= 3
+        answer += 1
+        
+        if (n<0):
+            print(-1)
+            break
+        
+solution()
