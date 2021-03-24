0a61273c27561c25286654b498d87bbb8cab009f
junsu hwang <junsu0325@naver.com>
2021-03-24 22:31:46 +0900

    Stack

---
+++ BOJ/Python/Stack/BOJ_9012.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10 ** 9)
+
+def solution():
+    t = int(sys.stdin.readline().rstrip())
+    
+    for _ in range(t):
+        vps = list(map(str, sys.stdin.readline().rstrip()))
+        
+        stack = []
+        flag = True
+
+        for string in vps:
+            if string == '(':
+                stack.append(string)
+            else:
+                if len(stack) == 0 or stack[len(stack) - 1] == ')':
+                    flag = False
+                else:
+                    stack.pop()
+        
+        if flag and len(stack) == 0:
+            print("YES")
+        else:
+            print("NO")
+
+solution()
