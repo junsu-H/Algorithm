44de23f6f4a9973b6897fc3ec2d3376770e58fb4
junsu hwang <junsu0325@naver.com>
2021-07-16 21:44:35 +0900

    Queue

---
+++ BOJ/Python/Queue/BOJ_1158.py
@@ -0,0 +1,17 @@
+# BOJ_1158 요세푸스 문제
+
+from sys import stdin
+
+input = stdin.readline
+
+def solution():
+    n, k = map(int, input().rstrip().split())
+    people = list(range(1, n+1))
+    answer, index = [], 0
+    
+    while people:
+        index = (index + (k-1)) % len(people)
+        answer.append(str(people.pop(index)))
+    
+    print("<{0}>".format(', '.join(answer)))    
+solution()
