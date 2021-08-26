5230a2cd8676bc950a25e913e180327d8ffabebb
junsu hwang <junsu0325@naver.com>
2021-08-26 22:40:50 +0900

    Bruteforcing

--- BOJ/Python/Bruteforcing/BOJ_7568.py
+++ BOJ/Python/Bruteforcing/BOJ_7568.py
@@ -6,14 +6,12 @@ input = stdin.readline
 
 def solution():
     n = int(input().rstrip())
-
     people = [list(map(int, input().rstrip().split())) for i in range(n)]
 
     for i in range(len(people)):
         rank = 1
 
-        for j in range(len(people)):
-            
+        for j in range(len(people)):           
             if not (people[i][0] == people[j][0] or people[i][1] == people[j][1]):
                 if people[i][0] < people[j][0] and people[i][1] < people[j][1]:
                     rank += 1
