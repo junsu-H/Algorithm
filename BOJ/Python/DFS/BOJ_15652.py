b8fcbd1310d61a7648582a639a1afccd8a43cc29
junsu hwang <junsu0325@naver.com>
2022-09-02 02:19:49 +0900

    DFS

--- BOJ/Python/DFS/BOJ_15652.py
+++ BOJ/Python/DFS/BOJ_15652.py
@@ -1,22 +1,21 @@
 # BOJ_15652 N과 M (4)
 
-from sys import setrecursionlimit
+from sys import setrecursionlimit, stdin
 
 setrecursionlimit(10**5)
 
-def dfs(level, number):
-  if level == M:
-    print(*answer)
-
-  else:
-      for i in range(number, N+1):
-            check[i] = 1
-            answer[level] = i
-            dfs(level+1, i)
-            check[i] = 0
+input = stdin.readline
 
 N, M = map(int, input().split())
 answer = [0] * M
-check = [0] * (N+1)
+
+def dfs(depth, number):
+	if depth == M:
+		print(*answer)
+	
+	else:
+		for i in range(number, N + 1):
+			answer[depth] = i
+			dfs(depth + 1, i)
 
 dfs(0, 1)
